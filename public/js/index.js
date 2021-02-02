c22deea3cf7f3b86c10ee263160fa981d36eae02
Nguyễn Đình Chiến <nguyendinhchienpc@gmail.com>
2021-02-02 10:38:29 +0700

    update

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+$(".clear").click(function(event){
+  $('input').val("");
+  $('textarea').val("");
+  return false
+})
--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -6,6 +6,9 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="css/index.css" />
 </head>
 <body>
     <div class="container">
@@ -45,10 +48,11 @@
             </div>
             <div class="send">
                 <button type="submit" class="btn btn-success">Gửi</button>
+                <button class="btn btn-primary clear">Clear</button>
             </div>
             @csrf
         </form>
     </div>
-    
+    <script src="js/index.js"></script>
 </body>
 </html>
